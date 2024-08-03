4d94d7a38efe86fdb86a57c6aabe00f660176bc0
Abhishek Kumar <141398481+Abhi75033@users.noreply.github.com>
2024-08-03 17:26:00 +0530

    Update Login.routes.js

--- Backend/src/routes/Login.routes.js
+++ Backend/src/routes/Login.routes.js
@@ -1,7 +1,6 @@
 
 import { Router } from "express";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import passport from "passport";
 import { LoginwithMail } from "../Controller/Login.Controller.js";
 
@@ -10,26 +9,22 @@ import { LoginwithMail } from "../Controller/Login.Controller.js";
 const router = Router()
 
 router.route('/send-mail').get(LoginwithMail)
-=======
 import { sendOtp, VerifyOtp } from "../Controller/Login.Controller.js";
-=======
+
 import passport from "passport";
 import { GooleLogin } from "../Controller/Login.Controller.js";
 
 
->>>>>>> ec7805a9 (Integrated googleauth successfully)
+
 
 const router = Router()
 
 
 
-<<<<<<< HEAD
->>>>>>> 7c9612f (Bakend Login setup has completed)
-=======
+
 router.route('/auth/google/callback').get(passport.authenticate('google',{
     successRedirect:'/dasboard',
     failureRedirect:'/login'
 }),GooleLogin)
->>>>>>> ec7805a9 (Integrated googleauth successfully)
 
 export default router
